db0d076c4604de9ca966e45cfbc640b21fb8af65
SIDDHARTH0823 <153288774+SIDDHARTH0823@users.noreply.github.com>
2024-01-04 11:20:59 +0530

    Add files via upload

---
+++ user.py
@@ -0,0 +1,11 @@
+T=int(input("Enter the value of temperature in Celcius"))
+H=int(input("Enter the value of humidity in percentage"))
+W=int(input("Enter the windspeed in km/hr"))
+WR= (0.5*T*T)-(0.2*H)+(0.1*W)-15
+print(WR)
+if WR > 300:
+  print("Sunny")
+elif 200<WR<=300:
+  print("Cloudy")
+else:
+  print("Rainy")

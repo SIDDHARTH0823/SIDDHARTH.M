93b7097bcef0e98e2f8933568169860c8c128b15
moola siddu <moolasiddu@moolas-MacBook-Air.local>
2024-01-04 21:35:09 +0530

    codes

---
+++ codes/mul.py
@@ -0,0 +1,14 @@
+X=(int(input("Enter the how many data sets you want the weather report for")))
+for i in range(X):
+
+ T=int(input("Enter the value of temperature in Celcius"))
+ H=int(input("Enter the value of humidity in percentage"))
+ W=int(input("Enter the windspeed in km/hr"))
+ WR= (0.5*T*T)-(0.2*H)+(0.1*W)-15
+ print(WR)
+ if WR > 300:
+  print("Sunny")
+ elif 200<WR<=300:
+  print("Cloudy")
+ else:
+  print("Rainy")
